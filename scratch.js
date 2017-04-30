d5d1b82e4c04f41d50b9a48ec9179ab71ee63e31
Stu Penrose <stu@penrose.us>
2017-04-30 00:13:59 -0700

    Switched to canvas

--- scratch.js
+++ scratch.js
@@ -28,16 +28,56 @@ function whenKeyPressed(key, fn){
 }
 
 
+
+var stage = document.getElementById('scratch-stage');
+//var stage = document.createElement("canvas");
+var ctx = stage.getContext("2d");
+document.body.appendChild(stage);
+
+var sprites = [];
+
+function redraw(){
+  console.log("Redraw " + sprites.length + " sprites");
+  ctx.clearRect(0, 0, stage.width, stage.height);
+  var x;
+  for(x=0;x<sprites.length;x++){
+    sprites[x].draw(ctx);
+  }
+}
+// http://creativejs.com/2012/01/day-10-drawing-rotated-images-into-canvas/
+var TO_RADIANS = Math.PI/180; 
+function drawRotatedImage(context, image, x, y, angle) { 
+ 
+	// save the current co-ordinate system 
+	// before we screw with it
+	context.save(); 
+ 
+	// move to the middle of where we want to draw our image
+	context.translate(x, y);
+ 
+	// rotate around that point, converting our 
+	// angle from degrees to radians 
+	context.rotate(angle * TO_RADIANS);
+ 
+	// draw it up and to the left by half the width
+	// and height of the image 
+	context.drawImage(image, -(image.width/2), -(image.height/2));
+ 
+	// and restore the co-ords to how they were when we began
+	context.restore(); 
+}
 function makeSprite(config){
 	var name = config.name;
-	var view = document.createElement("img");
+	var img = new Image();
 	var x, y;
 	var rotationDegrees = 0;
-	view.setAttribute("src", config.costume);
-	view.style.position = "absolute";
+	
 	setY(config.y);
 	setX(config.x);
-	document.body.appendChild(view);
+	
+	function draw(ctx){
+		drawRotatedImage(ctx, img, x, y, rotationDegrees);
+	}
 	
 	function changeXBy(x){
 		setX(getX() + x);
@@ -57,25 +97,22 @@ function makeSprite(config){
 	
 	function setX(newX){
 		x = newX;
-		view.style.left = x;
+		redraw();
 	}
 	function setY(newY){
 		y = newY;
-		view.style.top = y;
-	}
-	function whenClicked(fn){
-		view.addEventListener("click", fn);
+		redraw();
 	}
 	function rotateDegreesClockwise(n){
 		rotationDegrees = rotationDegrees + n;
-		view.style.transform = "rotate(" + rotationDegrees + "deg)"
+		redraw();
 	}
 	function rotateDegreesCounterClockwise(n){
 		rotationDegrees = rotationDegrees - n;
-		view.style.transform = "rotate(" + rotationDegrees + "deg)"
+		redraw();
 	}
-	return {
-		whenClicked:whenClicked, 
+	var me = {
+		draw:draw,
 		setY:setY,
 		setX:setX,
 		getX:getX,
@@ -84,4 +121,12 @@ function makeSprite(config){
 		changeYBy:changeYBy,
 		rotateDegreesClockwise:rotateDegreesClockwise,
 		rotateDegreesCounterClockwise:rotateDegreesCounterClockwise};
+	sprites.push(me);
+	
+	img.onload = function () {
+		redraw();
+	}
+	img.src = config.costume;
+	
+	return me;
 }
